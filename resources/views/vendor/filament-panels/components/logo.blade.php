c7044f049097c79e13aeb524f7394d3510023a86
ahmedsunil <iahmednill@gmail.com>
2023-11-28 00:19:32 +0500

    tweaking dashboard and AdminPanelProvider.php

---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,4 @@
+<div class="flex items-center space-x-6">
+    <img src="https://static-00.iconduck.com/assets.00/darth-vader-icon-2048x2005-8h9xt9tk.png" width="50" alt="">
+    <h1 class="font-bold text-gray-500">Starwars Encyclopedia</h1>
+</div>
